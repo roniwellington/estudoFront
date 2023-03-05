62f4ae7df82f0090d6d62e09df02d89d37f6783e
Roni Wellington <roniwellington@gmail.com>
2023-03-04 23:49:23 -0300

    estudo de front

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,4 +1,6 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
 robotron.addEventListener("click", function(){
     console.log('Clique no robô');
