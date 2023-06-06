42b35e89b44b8d09c7e1a2c77f9f0749804655ce
roniwellington <roniwellington@gmail.com>
2023-06-06 20:21:50 -0300

    estudo front

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -3,6 +3,7 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", ()=> {
     Evento.preventDefault()
     console.log("funcionou")
+    console.log("funcionou")
 })
 
 
