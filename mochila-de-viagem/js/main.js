3521d8f6cf3db73bf70783a2bc5906e71f18f553
roniwellington <roniwellington@gmail.com>
2023-06-13 23:48:29 -0300

    estudo front ent

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,11 +1,43 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = []
 
-form.addEventListener("submit", ()=> {
-    Evento.preventDefault()
+form.addEventListener("submit", (evento)=> {
+    evento.preventDefault()
 
-    console.log(Evento)
-    console.log(Evento.target.elements['nome'].value)
-    console.log(Evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
+
 })
+
+function criaElemento(nome, quantidade) {
+    //<li class="item"><strong>7</strong>Camisas</li>
+    const novoItem =  document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild (numeroItem )
+    novoItem.innerHTML += nome
+   
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+}
+
+
 
 
