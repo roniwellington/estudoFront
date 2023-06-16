cabff772df74d50c2d168daa9c6f3a84e0d82543
roniwellington <roniwellington@gmail.com>
2023-06-16 11:39:56 -0300

    estudo front

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento)=> {
     evento.preventDefault()
@@ -8,37 +12,35 @@ form.addEventListener("submit", (evento)=> {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value = ""
     quantidade.value = ""
 
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
     //<li class="item"><strong>7</strong>Camisas</li>
     const novoItem =  document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
 
     novoItem.appendChild (numeroItem )
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
    
     lista.appendChild(novoItem)
-
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    itens.push(itemAtual)
-
-    localStorage.setItem("item", JSON.stringify(itens))
-    itens.push(itemAtual)
-
-    localStorage.setItem("item", JSON.stringify(itens))
+   
 }
 
 
