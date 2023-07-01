eb67a3114f5f7de863984636470459189bb6ad1f
roniwellington <roniwellington@gmail.com>
2023-07-01 10:44:59 -0300

    estudo front

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,12 +1,27 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro){
-            throw Error('Esse cep não existe!')
-        } else 
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro){
+            throw Error('CEP não existente!');
+        };
+            var cidade = document.getElementById('cidade');
+            var logradoura = document.getElementById('endereco');
+            var estado = document.getElementById('estado');
 
-console.log(consultaCEP);
+            cidade.value = consultaCEPConvertida.localidade;
+            logradoura.value = consultaCEPConvertida.logradoura;
+            estado.value = consultaCEPConvertida.uf;
+
+            console.log(consultaCEPConvertida);
+            return consultaCEPConvertida;
+    } catch (erro){
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente</p> `;
+        console.log(erro)
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
