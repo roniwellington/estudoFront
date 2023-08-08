4179ba3f980fcdcec69dee05f5ce74ceb08ea14b
roniwellington <roniwellington@gmail.com>
2023-08-08 11:51:55 -0300

    Estudo de front

--- 2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
+++ 2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
@@ -1,43 +1,37 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-        <td class="td" data-td>${nome}</td>
-            <td>${email}</td>
-        <td>
-            <ul class="tabela__botoes-controle">
-                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-            </ul>
-        </td> `
-
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
+            return resposta.json()
+        })
 }
 
-const tabela = document.querySelector('[data-tabela]')
-
-const listaCliente = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-        http.open('GET', 'http://localhost:3000/profile')
-
-        http.onload = () => {
-            if (http.status >= 400) {
-                reject(JSON.parse(http.response))
-            } else {
-                resolve(JSON.parse(http.response))
-            }
-        }
-        http.send()
-    })
-    return promise
-
-}
-
-listaCliente()
-    .then(data => {
-        data.forEach(elemento => {
-            tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
         })
     })
+    .then(resposta => {
+        return resposta.body
+    })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente
+}
+
+
 
