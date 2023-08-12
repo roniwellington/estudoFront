3fa92e6ca57e4b3c1224ff2c94dee90e4cc11a4f
roniwellington <roniwellington@gmail.com>
2023-08-12 16:33:01 -0300

    estudo de front

--- 2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
+++ 2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
@@ -1,7 +1,11 @@
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
         .then(resposta => {
-            return resposta.json()
+            if(resposta.ok) {
+                return resposta.json()
+            }
+           
+            
         })
 }
 
@@ -17,20 +21,60 @@ const criaCliente = (nome, email) => {
         })
     })
     .then(resposta => {
-        return resposta.body
+        if (resposta.ok) {
+            return resposta.body
+        }
+        throw new Error ('Não foi possível listar os clientes')
     })
 }
 
 const removeCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE'
+    }).then( resposta => {
+        if(!resposta.ok) {
+            throw new Error ('Não foi possível remover o clientes')
+        }
+    })
+}
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => {
+        if (resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error ('Não foi possível detalhar o clientes')
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                nome: nome,
+                email: email
+            }
+        )
+    })
+    .then( resposta => {
+        if (resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error ('Não foi possível atualizar o clientes')
     })
 }
 
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
 
 
