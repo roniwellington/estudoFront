45765d7f51ca2f3f547730eef640793d7d7abe02
roniwellington <roniwellington@gmail.com>
2023-08-16 19:14:44 -0300

    estudo de front

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,10 +1,21 @@
 import { log } from "./utils/promise-helpers.js";
 import './utils/array-helpers.js';
 import { notasService as service } from "./nota/service.js";
+import { debounceTime, takeUntil, partialize, pipe } from "./utils/operators.js";
 
-document.querySelector('#myButton')
-    .onclick = () =>
-        service
-            .sumItems('2143')
-            .then(console.log)
-            .catch(console.log)
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+)
+
+const action = operations(() =>
+    service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log)
+
+);
+
+document
+    .querySelector('#myButton')
+    .onclick = action;
--- project/public/app/utils/operators.js
+++ project/public/app/utils/operators.js
@@ -9,3 +9,15 @@ export const pipe = (...fns) => value =>
     fns.reduce((previousValue, fn) =>
         fn(previousValue), value)
 
+
+export const takeUntil = (times, fn) => 
+    () => times-- > 0 && fn();
+
+
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer)
+        timer = setTimeout(fn, milliseconds);
+    }
+}
