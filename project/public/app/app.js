8bfe58b8aee66006697756a548a277993487b0b0
roniwellington <roniwellington@gmail.com>
2023-08-15 23:32:16 -0300

    estudo de front

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,14 +1,10 @@
-import { handleStatus, log } from "./utils/promise-helpers.js";
+import { log } from "./utils/promise-helpers.js";
 import './utils/array-helpers.js';
+import { notasService as service } from "./nota/service.js";
 
 document.querySelector('#myButton')
     .onclick = () =>
-        fetch('http://localhost:3000/notas')
-        .then(handleStatus)
-        .then(notas => notas
-            .$flatMap(nota => nota.itens)
-            .filter(item => item.codigo == '2143')
-            .reduce((total, item) => total + item.valor, 0)
-        )
-        .then(console.log)
-        .catch(console.log)
+        service
+            .sumItems('2143')
+            .then(console.log)
+            .catch(console.log)
