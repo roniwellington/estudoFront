8b9d5945968da9678719893826d54ae3247a3fab
roniwellington@gmail.com <roniwellington@gmail.com>
2023-09-18 13:20:03 -0300

    estudo de JS

--- js-heranca-polimorfismo-projetoinicial/Conta/Conta.js
+++ js-heranca-polimorfismo-projetoinicial/Conta/Conta.js
@@ -1,8 +1,13 @@
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta) {
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta Diretamente, pois essa é uma classe abstrata");
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+       
     }
 
     set cliente(novoValor) {
@@ -19,10 +24,9 @@ export class Conta {
         return this._saldo;
     }
 
-    sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa)
-
+    //Método abstrato
+    sacar(valor) {    
+        throw new Error("O método Sacar da conta é abstrato")
     }
 
     _sacar(valor, taxa) {
