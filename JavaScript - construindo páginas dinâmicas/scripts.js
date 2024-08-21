33db86b0c01b0b1078c327035c19d23861e19dd2
roniwellington <roniwellington@gmail.com>
2024-08-20 22:52:29 -0300

    estudo de paginas dinamicas e async await

---
+++ JavaScript - construindo páginas dinâmicas/scripts.js
@@ -0,0 +1,4 @@
+import { adicionarItem } from "./js/adicionarItem.js";
+
+const botaoSalvarItem = document.getElementById("adicionar-item");
+botaoSalvarItem.addEventListener("click", adicionarItem);
